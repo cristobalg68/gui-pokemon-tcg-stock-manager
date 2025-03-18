0a2cd1668955ca376f9564d030242571c6f374ab
cristobalg68 <cristobal.sebastian.guerra@gmail.com>
2025-03-18 18:52:47 -0300

    Changes de favicon.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,29 @@ import "./globals.css";
 export const metadata = {
   title: "GUI Pokemon TCG Stock Manager",
   description: "Graphical user interface that helps to visualize the stock management of a small store in tcgmatch.",
+  icons: {
+    icon: [
+      { 
+        url: "/favicon.ico",
+      },
+      {
+        url: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      { 
+        url: "/apple-icon.png",
+        type: "image/png" 
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
